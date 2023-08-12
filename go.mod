ce9f0670774e8843164eeb64df5cb84637175b65
faizin4107 <faizinfifaridillah92@gmail.com>
2023-08-13 00:11:22 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/faizin4107/app-go-say-hello2
+
+go 1.20
+
+require github.com/faizin4107/go-say-hello2 v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello "github.com/faizin4107/go-say-hello2"
+)
+
+func main() {
+	fmt.Println(say_hello.SayHello())
+}
